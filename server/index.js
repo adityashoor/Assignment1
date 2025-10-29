41c86f2c2fb02f932e6b8261448b341d361e36c6
AdityaShoor <adityashoor87@gmail.com>
2025-10-29 11:12:07 -0400

    feat(server): add root GET / that returns 'hello world'

--- server/index.js
+++ server/index.js
@@ -23,6 +23,11 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB:', MONGO_URI))
   .catch(err => console.error('MongoDB connection error:', err));
 
+// Simple root route to verify server is running
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
 // Models
 const Contact = require('./models/Contact');
 const Project = require('./models/Project');
